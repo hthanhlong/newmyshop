25fb3ab48b589eeaff8222d774dad2e360db9245
Your Name <hthanhlong97@gmail.com>
2020-09-21 13:01:30 +0700

    try hard

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 app.use(express.json());
 
 mongoose.connect(
-  process.env.MONGODB_URL ||
+  process.env.MONGODB_URI ||
     "mongodb+srv://thanhlong123:thanhlong@cluster0.6vknj.mongodb.net/thanhlong123?retryWrites=true&w=majority ",
   {
     useNewUrlParser: true,
